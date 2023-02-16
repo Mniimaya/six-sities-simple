6201102eed70485f9b543ff3052ecc6d60cb781e
Mnimaya <mnimayaaa@gmail.com~>
2023-02-16 10:46:07 +0300

    test

--- project/src/components/cities/place-card/place-card.jsx
+++ project/src/components/cities/place-card/place-card.jsx
@@ -1,32 +1,32 @@
-export default function PlaceCard() {
+export default function PlaceCard(props) {
     return(
         <article className="cities__card place-card">
         <div className="place-card__mark">
-          <span>Premium</span>
+          <span>{props.class}</span>
         </div>
         <div className="cities__image-wrapper place-card__image-wrapper">
           <a href="text.html">
-            <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place imagezfv"/>
+            <img className="place-card__image" src={props.src} width="260" height="200" alt="Place imagezfv"/>
           </a>
         </div>
         <div className="place-card__info">
           <div className="place-card__price-wrapper">
             <div className="place-card__price">
-              <b className="place-card__price-value">&euro;120</b>
+              <b className="place-card__price-value">&euro;{props.price}</b>
               <span className="place-card__price-text">&#47;&nbsp;night</span>
             </div>
 
           </div>
           <div className="place-card__rating rating">
             <div className="place-card__stars rating__stars">
-              <span style={{width:'80%'}}></span>
+              <span style={{width: `${props.rating}`}}></span>
               <span className="visually-hidden">Rating</span>
             </div>
           </div>
           <h2 className="place-card__name">
-            <a href="text.html">Beautiful &amp; luxurious apartment at great location</a>
+            <a href="text.html">{props.cardName}</a>
           </h2>
-          <p className="place-card__type">Apartment</p>
+          <p className="place-card__type">{props.type}</p>
         </div>
       </article>
     );
--- project/src/components/places-list/places-list.jsx
+++ project/src/components/places-list/places-list.jsx
@@ -1,9 +1,55 @@
 import PlaceCard from "../cities/place-card/place-card";
 
+const DATA_PLACES_ITEMS = [
+    {   key: "1",
+        price: "120",
+        cardName: "Beautiful &amp; luxurious apartment at great location",
+        type: "Apartment",
+        src: "img/apartment-01.jpg",
+        class: "Premium",
+        rating: "80%"
+    },
+
+    {   key: "2",
+        price: "80",
+        cardName: "Wood and stone place",
+        type: "Private room",
+        src: "img/room.jpg",
+        class: "",
+        rating: "80%"
+    },
+    {   key: "3",
+        price: "132",
+        cardName: "Canal View Prinsengracht",
+        type: "Apartment",
+        src: "img/apartment-02.jpg",
+        class: "",
+        rating: "100%"
+    },
+    {   key: "4",
+        price: "180",
+        cardName: "Nice, cozy, warm big bed apartment",
+        type: "Apartment",
+        src: "img/apartment-03.jpg",
+        class: "Premium",
+        rating: "80%"
+    },
+    {   key: "5",
+        price: "80",
+        cardName: "Wood and stone place",
+        type: "Private room",
+        src: "img/room.jpg",
+        class: "",
+        rating: "80%"
+    },
+];
+
 export default function PlacesList() {
+        let itemArray = DATA_PLACES_ITEMS.map((item) => <PlaceCard key={item.key} price={item.price} cardName={item.cardName} type={item.type} class={item.class} src={item.src}/>)
+
     return(
         <div className="cities__places-list places__list tabs__content">
-            <PlaceCard />
+            {itemArray}
       </div>
     );
 }
